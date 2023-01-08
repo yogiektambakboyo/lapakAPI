7c31859013223fd2b711b50fc4f918379cb8cc18
Yogi Aditya <it.yogi@borwita.co.id>
2023-01-08 20:45:28 +0700

    m

--- api_v1.go
+++ api_v1.go
@@ -534,9 +534,9 @@ func setupRouter() *gin.Engine {
 			log.Fatal(err1)
 		}
 
-		sqlstring = "INSERT INTO public.sales_trip(dated, sales_id, time_start, time_end, active, created_by, created_at, photo, notes) VALUES(now()::date, $1, now(), now(), '1', $2, now(), $3, $4);"
+		sqlstring = "INSERT INTO public.sales_trip(dated, sales_id, time_start, time_end, active, created_by, created_at, photo, notes) VALUES(now()::date, $1, now(), now(), '1', $1, now(), $2, $3);"
 
-		rows2, err2 := db.Query(sqlstring,xsales_id,xsales_id,xphoto,xnotes)
+		rows2, err2 := db.Query(sqlstring,xsales_id,xphoto,xnotes)
 		defer rows2.Close()
 		if err2 != nil {
 			log.Fatal(err2)
